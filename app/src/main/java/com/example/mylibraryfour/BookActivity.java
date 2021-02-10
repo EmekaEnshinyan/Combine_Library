da28e7cf3cc99b472cd19860ca3198e83e71cec5
Enshinyan <emeka.enshinyan@gmail.com>
2021-02-10 12:11:47 -0800

    Made single book activity

--- app/src/main/java/com/example/mylibraryfour/BookActivity.java
+++ app/src/main/java/com/example/mylibraryfour/BookActivity.java
@@ -3,12 +3,55 @@ package com.example.mylibraryfour;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class BookActivity extends AppCompatActivity {
-
+    private ImageView bookImage;
+    private Button btnAddCurrentlyReading, btnAddWantRead, btnAddAlreadyRead, btnAddFavorites;
+    private TextView nameText, txtBookName, authorText, txtAuthorName, pagesText, txtPages, descriptionText,
+            txtDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+
+        InitViews();
+        //TODO: get data from recview
+        Book book = new Book(1, "book name", "author 2", 100, "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg",
+                "Test 2", "Long Desc");
+        setData(book);
+
+    }
+
+    private void InitViews(){
+        bookImage = findViewById(R.id.imgBook);
+
+        btnAddCurrentlyReading = findViewById(R.id.btnAddCurrentlyReading);
+        btnAddWantRead = findViewById(R.id.btnAddWantRead);
+        btnAddAlreadyRead = findViewById(R.id.btnAddAlreadyRead);
+        btnAddFavorites = findViewById(R.id.btnAddFavorites);
+
+        nameText = findViewById(R.id.nameText);
+        txtBookName = findViewById(R.id.txtBookName);
+        authorText = findViewById(R.id.authorText);
+        txtAuthorName = findViewById(R.id.txtAuthorName);
+        pagesText = findViewById(R.id.pagesText);
+        txtPages = findViewById(R.id.txtPages);
+        descriptionText = findViewById(R.id.descriptionText);
+        txtDescription = findViewById(R.id.txtDescription);
+    }
+
+    private void setData(Book book){
+        txtBookName.setText(book.getName());
+        txtAuthorName.setText(book.getAuthor());
+        txtPages.setText(String.valueOf(book.getPages()));
+        txtDescription.setText(book.getLongDesc());/*
+        Glide.with(this)
+                .asBitmap().load(book.getImageUrl())
+                .into(book.getImageUrl());*/
     }
 }
--- app/src/main/java/com/example/mylibraryfour/BookRecViewAdapter.java
+++ app/src/main/java/com/example/mylibraryfour/BookRecViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.mylibraryfour;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -49,8 +50,8 @@ public class BookRecViewAdapter extends RecyclerView.Adapter<BookRecViewAdapter.
         holder.parent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(mContext, books.get(position).getName() + "Selected", Toast.LENGTH_SHORT).show();
-
+                Intent intent = new Intent(mContext, BookActivity.class);
+                mContext.startActivity(intent);
             }
         });
 
