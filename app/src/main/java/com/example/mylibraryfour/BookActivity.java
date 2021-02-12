9dfe77341c2b2a70d925250c2b144aa53a39ad49
Enshinyan <emeka.enshinyan@gmail.com>
2021-02-12 10:32:35 -0800

    adding actions for alreadyread btn

--- app/src/main/java/com/example/mylibraryfour/BookActivity.java
+++ app/src/main/java/com/example/mylibraryfour/BookActivity.java
@@ -2,25 +2,30 @@ package com.example.mylibraryfour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
+
 public class BookActivity extends AppCompatActivity {
     private ImageView bookImage;
     private Button btnAddCurrentlyReading, btnAddWantRead, btnAddAlreadyRead, btnAddFavorites;
     private TextView nameText, txtBookName, authorText, txtAuthorName, pagesText, txtPages, descriptionText,
             txtDescription;
+    public static final String BOOK_ID_KEY = "bookId";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
 
         InitViews();
-        String longDesc = "Dune is set in the distant future amidst a feudal " +
+/*        String longDesc = "Dune is set in the distant future amidst a feudal " +
                 "\ninterstellar society in which various noble houses control " +
                 "\nplanetary fiefs. It tells the story of young Paul Atreides, " +
                 "\nwhose family accepts the stewardship of the planet Arrakis. " +
@@ -37,11 +42,44 @@ public class BookActivity extends AppCompatActivity {
                 "\nfor the control of Arrakis and its spice.";
         //TODO: get data from recview
         Book book = new Book(1, "Dune", "Frank Herbert", 300, "https://images-na.ssl-images-amazon.com/images/I/91Fq9Amx0pL.jpg",
-                "Test 1", longDesc);
-        setData(book);
+                "Test 1", longDesc);*/
 
+        //passing default value as -1 since none of books in list will have -1
+        Intent intent = getIntent();
+        if (null != intent){
+            int bookId = intent.getIntExtra(BOOK_ID_KEY, -1);
+            if (bookId != -1){
+                //3 security checks: checking intent is not null, make sure data received from intent, and checked if there is such a book in a list
+                //ALWAYS CHECK FOR INTENT AND INTENT DATA
+                Book incomingBook = Utils.getInstance().getBookById(bookId);
+                if (null != incomingBook){
+                    setData(incomingBook);
+
+                    handleAlreadyRead(incomingBook);
+                }
+            }
+        }
     }
+    public void handleAlreadyRead(Book book){
+        ArrayList<Book> alreadyReadBooks = Utils.getInstance().getAlreadyReadBooks();
+        Boolean existingAlreadyReadBooks = false;
+        for (Book b: alreadyReadBooks){
+            if (b.getId() == book.getId()){
+                existingAlreadyReadBooks = true;
+            }
+        }
+        if (existingAlreadyReadBooks){
+             btnAddAlreadyRead.setEnabled(false);
 
+        }else{
+            btnAddAlreadyRead.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+    }
     private void InitViews(){
         bookImage = findViewById(R.id.imgBook);
 
--- app/src/main/java/com/example/mylibraryfour/Utils.java
+++ app/src/main/java/com/example/mylibraryfour/Utils.java
@@ -7,33 +7,35 @@ public class Utils {
 
     private static Utils instance;
 
-    private Utils(){
-        if (null == allBooks){
+    private Utils() {
+        if (null == allBooks) {
             allBooks = new ArrayList<>();
             InitData();
         }
-        if (null == alreadyReadBooks){
+        if (null == alreadyReadBooks) {
             alreadyReadBooks = new ArrayList<>();
         }
-        if (null == wantToReadBooks){
+        if (null == wantToReadBooks) {
             wantToReadBooks = new ArrayList<>();
         }
-        if (null == currentlyReadingBooks){
+        if (null == currentlyReadingBooks) {
             currentlyReadingBooks = new ArrayList<>();
         }
-        if (null == favoriteBooks){
+        if (null == favoriteBooks) {
             favoriteBooks = new ArrayList<>();
         }
     }
-    private void InitData(){
+
+    private void InitData() {
         //TODO: add initial data
         ArrayList<Book> books = new ArrayList<>();
         //check if adding a jpeg from the internet causes a crash
         allBooks.add(new Book(1, "Dune", "Frank Herbert", 300, "https://images-na.ssl-images-amazon.com/images/I/91Fq9Amx0pL.jpg",
                 "Test 1", "long description"));
-        allBooks.add(new Book(1, "book name", "author 2", 100, "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg",
+        allBooks.add(new Book(2, "book name", "author 2", 100, "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg",
                 "Test 2", "Long Desc"));
     }
+
     private static ArrayList<Book> allBooks;
     private static ArrayList<Book> alreadyReadBooks;
     private static ArrayList<Book> wantToReadBooks;
@@ -69,5 +71,20 @@ public class Utils {
 
     public static ArrayList<Book> getFavoriteBooks() {
         return favoriteBooks;
+    }
+
+    //may need to remove null and modify the code
+    public Book getBookById(int id) {
+        for (Book b: allBooks) {
+            if (b.getId() == id) {
+                return b;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean addToAlreadyRead(Book book){
+        return alreadyReadBooks.add(book);
     }
 }
