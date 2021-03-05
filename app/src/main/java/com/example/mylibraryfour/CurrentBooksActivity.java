e7336393d94b3444c0ded7cce32eae83c8ed7f39
Enshinyan <emeka.enshinyan@gmail.com>
2021-03-04 23:38:38 -0800

    currentBooks activity made

---
+++ app/src/main/java/com/example/mylibraryfour/CurrentBooksActivity.java
@@ -0,0 +1,14 @@
+package com.example.mylibraryfour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentBooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_books);
+    }
+}
